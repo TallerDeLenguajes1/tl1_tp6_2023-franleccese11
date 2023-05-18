413ba3a03042371bffafeb0feb9b1cfe706b1cd0
franleccese11 <franchelaleccese@gmail.com>
2023-05-18 19:12:20 -0300

    comitia pibe

---
+++ practico6/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int a;
+int b;
+a= 10;
+b=a;
+Console.WriteLine("valor de a:"+a);
+Console.WriteLine("valor de b:"+b);
+
+//ejercicio 1
+
+float num1=0;
+bool exito = false;
+string? numString = "";
+while(!exito)
+{
+    Console.WriteLine("ingrese un numero valido:");
+    numString = Console.ReadLine();
+    exito = float.TryParse(numString,out num1);
+    if (!exito)
+    {
+        Console.WriteLine(numString + "no es un numero valido");
+    }
+} 
+
